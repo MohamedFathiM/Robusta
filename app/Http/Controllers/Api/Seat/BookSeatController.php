61621cdc176358c558931980d8208841b408d44c
MohamedFathi <mohamedkdr66@yahoo.com>
2023-03-11 08:28:34 +0200

    make seat is booked on specific trip when reserving it

--- app/Http/Controllers/Api/Seat/BookSeatController.php
+++ app/Http/Controllers/Api/Seat/BookSeatController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Seat;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Seat\BookingRequest;
 use App\Http\Resources\Api\Seat\SeatResource;
+use App\Models\Seat;
 use App\Models\UserBooking;
 use App\Support\Traits\ApiResponse;
 
@@ -14,15 +15,23 @@ class BookSeatController extends Controller
 
     public function __invoke(BookingRequest $request)
     {
-        if (UserBooking::where([
-            'seat_id' => $request->seat_id,
-            'station_id' => $request->station_id,
-            'user_id' => $request->user_id
-        ])->exists()) {
+        if (
+            UserBooking::where([
+                'seat_id' => $request->seat_id,
+                'station_id' => $request->station_id,
+                'user_id' => $request->user_id
+            ])->exists()
+            ||
+            Seat::where([
+                'id' => $request->seat_id,
+                'is_booking' => true
+            ])->exists()
+        ) {
             return $this->errorResponse(message: 'This seat is not available');
         }
 
         $userBooking = UserBooking::create($request->validated());
+        Seat::find($request->seat_id)->update(['is_booking' => true]);
 
         return $this->successResponse(SeatResource::make($userBooking->seat), message: 'Congrate !');
     }
